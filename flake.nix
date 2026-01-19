99332a36783030f744e2c2d937ba47b3e5b8a44c
blazejOz <blazej.ozog@gmail.com>
2026-01-19 06:00:11 +0100

    Add: build tools - cmake/flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Pico 2 W Environment - Full SDK";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    pico-sdk-src = {
+      url = "git+https://github.com/raspberrypi/pico-sdk?ref=master&submodules=1";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, pico-sdk-src }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cmake
+          gcc-arm-embedded
+          python3
+          picotool
+        ];
+
+        shellHook = ''
+          export PICO_SDK_PATH="${pico-sdk-src}"
+
+          echo "🚀 Pico 2 W Environment Active (Full SDK with Submodules)"
+        '';
+      };
+    };
+}
